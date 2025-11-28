f95151d22038556137269e4437321a308175f9a5
kelle <kelle@SCHMOM_DS>
2025-11-28 13:44:39 +0100

    mhhh

--- BagBeasts/src/Item/PermanentItemBase.cs
+++ BagBeasts/src/Item/PermanentItemBase.cs
@@ -1,14 +1,8 @@
-using src.Item.ItemBase;
-
-namespace src.Item.PermanentItemBase;
-
-public class PermanentItemBase : ItemBase
+namespace src.Item.ItemBase;
+public abstract class PermanentItemBase : ItemBase
 {
     #region Methods
-
-    public abstract void ItemEffect(BagBeast holderBeast)
-    {
-    }
+    public abstract void ItemEffect(BagBeast holderBeast);
 
     #endregion //Methods
 }

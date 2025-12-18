55ce3da0f57fab7a082bfec2edf6705b8cfd3c77
kelle <kelle@SCHMOM_DS>
2025-12-18 09:53:55 +0100

    änderung auf Postgres und EF

---
+++ BagBeasts/src/Database/PostgresContext.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using BagBeasts;
+using BagBeasts.src.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace BagBeasts.src.Database;
+
+public partial class PostgresContext : DbContext
+{
+    public PostgresContext()
+    {
+    }
+
+    public PostgresContext(DbContextOptions<PostgresContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Ability> Abilities { get; set; }
+
+    public virtual DbSet<Bagbeasts> Bagbeasts { get; set; }
+
+    public virtual DbSet<Item> Items { get; set; }
+
+    public virtual DbSet<Move> Moves { get; set; }
+
+    public virtual DbSet<Type> Types { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=simon;Persist Security Info=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ability>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("abilities_pkey");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Bagbeasts>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Id");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.Atk)
+                .HasDefaultValueSql("0")
+                .HasColumnName("atk");
+            entity.Property(e => e.Def).HasColumnName("def");
+            entity.Property(e => e.Hp).HasColumnName("hp");
+            entity.Property(e => e.Initiative).HasColumnName("initiative");
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Spa).HasColumnName("spa");
+            entity.Property(e => e.Spd).HasColumnName("spd");
+            entity.Property(e => e.Type1).HasColumnName("type_1");
+            entity.Property(e => e.Type2).HasColumnName("type_2");
+
+            entity.HasMany(d => d.Moves).WithMany(p => p.Bagbeasts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "BagbeastMove",
+                    r => r.HasOne<Move>().WithMany()
+                        .HasForeignKey("MoveId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("MoveId"),
+                    l => l.HasOne<Bagbeasts>().WithMany()
+                        .HasForeignKey("BagbeastId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("Bagbeastid"),
+                    j =>
+                    {
+                        j.HasKey("BagbeastId", "MoveId").HasName("BMId");
+                        j.ToTable("BagbeastMoves");
+                    });
+        });
+
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("items_pkey");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Move>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("moves_pkey");
+
+            entity.Property(e => e.Id)
+                .UseIdentityAlwaysColumn()
+                .HasColumnName("id");
+            entity.Property(e => e.Description).HasColumnName("description");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Type>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Types_pkey");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("pk_uid");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Id).UseIdentityAlwaysColumn();
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Psw).HasColumnName("psw");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BagBeasts/src/Database/datareader.cs
@@ -0,0 +1,164 @@
+﻿using BagBeasts.src.Beast;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Xml.Linq;
+
+namespace BagBeasts.src.Database
+{
+    /// <summary>
+    /// Reader der Postgres Datenbank
+    /// </summary>
+    public class Datareader
+    {
+        /// <summary>
+        /// Holt sich alle Bagbeasts von der Datenbank
+        /// </summary>
+        /// <returns></returns>
+        public List<BagBeast> GetBagBeasts()
+        {
+            try
+            {
+                using (PostgresContext context = new PostgresContext())
+                {
+                    List<BagBeast> retval = new();
+                    foreach (var beast in context.Bagbeasts)
+                    {
+                        Type typ1 = new Type
+                        {
+                            Id = beast.Type1
+                        };
+                        if (beast.Type2 != null)
+                        {
+                            Type typ2 = new Type
+                            {
+                                Id = beast.Type2.Value
+                            };
+                            retval.Add(new BagBeast(beast.Name, beast.Hp, beast.Atk, beast.Spa, beast.Def, beast.Spd, beast.Initiative, typ1, typ2));
+                        }
+                        else
+                        {
+                            retval.Add(new BagBeast(beast.Name, beast.Hp, beast.Atk, beast.Spa, beast.Def, beast.Spd, beast.Initiative, typ1));
+                        }
+                    }
+                    return retval;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Holt sich alle Abilities von der Datenbank
+        /// </summary>
+        /// <returns>Liste der Abilities</returns>
+        public List<Ability> GetAblities()
+        {
+            try
+            {
+                using (PostgresContext context = new PostgresContext())
+                {
+                    List<Ability> retval = new();
+                    foreach (var ability in context.Abilities)
+                    {
+                        if (ability.Name != null)
+                        {
+                            retval.Add(new Ability(ability.Id, ability.Name));
+                        }
+                        else
+                        {
+                            retval.Add(new Ability(ability.Id));
+                        }
+                    }
+                    return retval;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Holt sich alle Moves von der Datenbank
+        /// </summary>
+        /// <returns>Liste der Moves</returns>
+        public List<Move> GetMoves()
+        {
+            try
+            {
+                using (PostgresContext context = new PostgresContext())
+                {
+                    List<Move> retval = new();
+                    foreach (var moves in context.Moves)
+                    {
+                        retval.Add(moves);
+                    }
+                    return retval;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Holt sich alle Moves zu einem Vieh von der Datenbank
+        /// </summary>
+        /// <param name="referenz">BeagBeast</param>
+        /// <returns>Liste der Moves</returns>
+        public List<Move> GetMovesRef(BagBeast referenz)
+        {
+            try
+            {
+                using (PostgresContext context = new PostgresContext())
+                {
+                    List<Move> retval = new();
+                    foreach (Move moves in context.Bagbeasts.Where(x => x.Id == referenz.ID).First().Moves)
+                    {
+                        retval.Add(moves);
+                    }
+                    return retval;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Holt alle Items von der Datenbank
+        /// </summary>
+        /// <returns>Liste der Items</returns>
+        public List<Item> GetItems()
+        {
+            try
+            {
+                using (PostgresContext context = new PostgresContext())
+                {
+                    List<Item> retval = new();
+                    foreach (Item item in context.Items)
+                    {
+                        retval.Add(item);
+                    }
+                    return retval;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return null;
+        }
+    }
+}
